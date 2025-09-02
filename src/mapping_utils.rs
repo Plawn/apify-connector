70c0cda6e8720d27d3398866f2b665349d0a62a6
Plawn <plawn@konf.sh>
2025-09-02 11:05:33 +0200

    with scripting

--- src/mapping_utils.rs
+++ src/mapping_utils.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
+use rhai::{Engine, Scope};
 use serde_json::Value;
 
 use crate::dto::{ExportItem, JobCreation};
@@ -15,20 +16,28 @@ pub fn update_state(
     job: &JobCreation,
     ctx: &Context,
 ) -> anyhow::Result<String> {
+    // state should be parsed once
     let mut state: HashMap<String, Value> = serde_json::from_str(&job.state)?;
-
+    // engine should be created once
+    let mut engine = Engine::new();
+    let mut scope = Scope::new();
+    scope.push("start_date", ctx.start);
+    engine.register_fn("sub_days_and_format", |d: DateTime<Utc>, days: i64| {
+        let res = d - Duration::days(days);
+        res.format("%Y-%m-%d").to_string()
+    });
     if let Some(mapping) = &job.settings.state_mapping {
         for m in mapping {
-            match m.update.as_str() {
-                "$start_date" => {
-                    let k = &m.from;
-                    let date_str = ctx.start.format("%Y-%m-%d").to_string();
-                    state.insert(k.into(), Value::String(date_str));
-                }
-                _ => {
-                    anyhow::bail!("can't find transformation")
-                }
-            }
+            let result = if m.update.starts_with("$") {
+                let s = &m.update[1..];
+                let result: String = engine
+                    .eval_with_scope(&mut scope, s)
+                    .unwrap();
+                result
+            } else {
+                m.from.to_string()
+            };
+            state.insert(m.from.to_string(), Value::String(result));
         }
     }
 
